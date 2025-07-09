252a52bea682666573e065a18c70ecfc5be96975
osama-jeb <oussamajebrane98@gmail.com>
2025-07-09 16:26:38 +0100

    top section base

---
+++ src/sections/about.tsx
@@ -0,0 +1,14 @@
+export default function About() {
+    return (
+        <section className="min-h-[60vh] bg-amber-800 flex items-center justify-center">
+            <div className="bg-purple-800 w-full px-16">
+                <p className="text-3xl">About</p>
+
+                <div className="flex items-center gap-2">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est vel quasi harum non voluptatem accusantium. Veritatis cum reprehenderit quod, non quia, rerum deleniti consectetur, eum beatae similique dolore. Qui, cupiditate.</p>
+                    <p>img</p>
+                </div>
+            </div>
+        </section>
+    )
+}
---
+++ src/sections/navbar.tsx
@@ -0,0 +1,22 @@
+import logo from "../assets/images/favicon-_1_.svg";
+
+export default function Navbar() {
+
+    return (
+        <nav className="flex items-center justify-between bg-red-400 p-4">
+            <div className="flex items-center gap-2">
+                <img src={logo} alt="portfolio_logo"
+                    className="w-12 aspect-square object-cover"
+                />
+                <p>Oussama Jebrane</p>
+            </div>
+
+            <div className="flex items-center gap-2">
+                <p>About</p>
+                <p>Skills</p>
+                <p>Projects</p>
+                <p>Contact</p>
+            </div>
+        </nav>
+    )
+}
